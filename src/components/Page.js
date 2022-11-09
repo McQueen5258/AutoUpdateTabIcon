6331465db1bbe40df4ce7322138a1694ab38bb63
Eric <49057152+McQueen5258@users.noreply.github.com>
2022-11-09 17:44:40 +0800

    testing delete share icon(10)

--- src/components/Page.js
+++ src/components/Page.js
@@ -3,19 +3,19 @@ import React, { useEffect } from "react";
 function Page({ text = "NoFound" }) {
   const initializePage = () => {
     // title
-    document.title = `Page${text}`;
+    // document.title = `Page${text}`;
     const logoUrl = `/images/${text}.PNG`;
 
     // favicon
     let link = document.querySelector("link[rel~='icon']");
     if (link) {
-      link.href = logoUrl;
+      // link.href = logoUrl;
     }
 
     // apple touch icon
     let linkApple = document.querySelector("link[rel~='apple-touch-icon']");
     if (linkApple) {
-      linkApple.href = logoUrl;
+      // linkApple.href = logoUrl;
     }
   };
   useEffect(() => {
